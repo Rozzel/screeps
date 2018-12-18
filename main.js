98b6a23f2ce453df9af687ec2e81bf3ca8f2b606
Vladislav Golik <Vladislav.Golik@gmail.com>
2018-12-18 23:53:13 +0300

    HarvesterBig
    Хороший мальчик

--- main.js
+++ main.js
@@ -21,9 +21,13 @@ module.exports.loop = function () {
         }
     }
 
-    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1', { memory: { role: 'builder' } } );
     Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1', { memory: { role: 'harvester' } } );
+    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1', { memory: { role: 'builder' } } );
     Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1', { memory: { role: 'upgrader' } } );
+
+    Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
+        'HarvesterBig',
+        { memory: { role: 'harvester' } } );
 }
 
 
