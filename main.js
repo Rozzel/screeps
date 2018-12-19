975530d19fb870ec85e9cd77f970019b46cd0253
Vladislav <rozzel@mail.ru>
2018-12-19 17:17:35 +0300

    spawningCreep

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ var spawningPool = function (droneName, droneRole, maxAmount, droneBody) {
     if(Game.spawns['Spawn1'].spawning) { 
         var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
         Game.spawns['Spawn1'].room.visual.text(
-            '🛠️' + spawningCreep.memory.role,
+            spawningCreep.memory.role,
             Game.spawns['Spawn1'].pos.x + 1, 
             Game.spawns['Spawn1'].pos.y, 
             {align: 'left', opacity: 0.8});
