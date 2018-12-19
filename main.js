8fe04e55bb88a61a1a52d4b296603fb85b846133
Vladislav Golik <Vladislav.Golik@gmail.com>
2018-12-19 09:28:25 +0300

    'Upgrader1

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ module.exports.loop = function () {
 
     Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1', { memory: { role: 'harvester' } } );
     Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Builder1', { memory: { role: 'builder' } } );
-    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1', { memory: { role: 'upgrader' } } );
+    Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester2', { memory: { role: 'harvester' } } );
+    Game.spawns['Spawn1'].spawnCreep( [WORK, WORK, CARRY, MOVE, CARRY, CARRY], 'Upgrader1', { memory: { role: 'upgrader' } } );
 
     Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
         'HarvesterBig',
