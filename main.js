660da832f226d90d0d076b053a7cb4e9b9d1e46d
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-01-27 10:08:09 +0300

    harvester-8

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ module.exports.loop = function () {
 var controllerLevel = Game.rooms['W36N43'].controller.level;
 
 if (controllerLevel > 1) {
-    spawningPool('droneHarvesterBig', 'harvester', 6, [WORK,WORK,MOVE,MOVE,CARRY,MOVE,MOVE]);
+    spawningPool('droneHarvesterBig', 'harvester', 8, [WORK,WORK,MOVE,MOVE,CARRY,MOVE,MOVE]);
     spawningPool('droneUpgraderBig', 'upgrader', 12, [WORK,WORK,CARRY,MOVE,MOVE,MOVE]);
     spawningPool('droneBuilder', 'builder', 0, [WORK,WORK,CARRY,MOVE]);
 
