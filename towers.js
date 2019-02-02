551be9ad6fe227ae93cd9d4de9b46dabe9e18c88
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-02 21:47:31 +0300

    sporeCrawlerTower

--- towers.js
+++ towers.js
@@ -4,13 +4,12 @@ var sporeCrawlerTower = function () {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
-
         var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        
         if(closestHostile) {
             tower.attack(closestHostile);
+        } else {
+            tower.repair(closestDamagedStructure);
         }
     }
 };
