31b5068715f621e50d550d798310a9200436581c
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-02 21:52:33 +0300

    sporeCrawlerTower

--- towers.js
+++ towers.js
@@ -1,17 +1,17 @@
 var sporeCrawlerTower = function () {
     var tower = Game.getObjectById('5c55df5e6f956a230b9ebfe8');
-    if(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        } else {
-            tower.repair(closestDamagedStructure);
-        }
+
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax
+    });
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+
+    if (closestHostile) {
+        tower.attack(closestHostile);
+    } else {
+        tower.repair(closestDamagedStructure);
     }
+
 };
 
 module.exports = sporeCrawlerTower;
