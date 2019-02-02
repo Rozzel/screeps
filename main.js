2cc08fa64321b35e98aedafb52bc892f8f60a633
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-02 20:28:41 +0300

    spawningPool

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleBuilder = require('role.builder');
 var roleUpgrader = require('role.upgrader');
+var spawningPool = require('spawning');
 
 module.exports.loop = function () { 
 
@@ -11,21 +12,13 @@ module.exports.loop = function () {
 spawningPool('droneUpgraderBig', 'upgrader', 0, [WORK,WORK,CARRY,MOVE,MOVE,MOVE]);
 spawningPool('droneBuilderBig', 'builder', 1, [WORK,WORK,CARRY,MOVE]);
 spawningPool('droneUpgrader', 'upgrader', 1, [WORK,WORK,CARRY,MOVE]);
-spawningPool('droneHarvester', 'harvester', 5, [WORK,CARRY,MOVE]);
-spawningPool('droneHarvesterBig', 'harvester', 2, [WORK,MOVE,CARRY,CARRY]);
-spawningPool('droneBuilder', 'builder', 3, [WORK,CARRY,MOVE]);
+spawningPool('droneHarvester', 'harvester', 12, [WORK,CARRY,MOVE]);
+spawningPool('droneHarvesterBig', 'harvester', 0, [WORK,MOVE,CARRY,CARRY]);
+spawningPool('droneBuilder', 'builder', 4, [WORK,CARRY,MOVE]);
 
 unitsRole();
 
 
-}
-
-var createExtension = function (x, y) {
-    Game.spawns['Hatchery'].room.createConstructionSite( x - 2, y,      STRUCTURE_EXTENSION );
-    Game.spawns['Hatchery'].room.createConstructionSite( x + 2, y,      STRUCTURE_EXTENSION );
-    Game.spawns['Hatchery'].room.createConstructionSite( x    , y - 2,  STRUCTURE_EXTENSION );
-    Game.spawns['Hatchery'].room.createConstructionSite( x - 1, y + 2,  STRUCTURE_EXTENSION );
-    Game.spawns['Hatchery'].room.createConstructionSite( x + 1, y + 2,  STRUCTURE_EXTENSION );
 }
 
 var sporeCrawler = function () {
@@ -50,7 +43,6 @@ var deleteCreepsMemory = function () {
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
         }
     }
 }
@@ -71,22 +63,3 @@ var unitsRole = function () {
     }
 }
 
-var spawningPool = function (droneName, droneRole, maxAmount, droneBody) {
-    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == droneRole);
-
-    if(harvesters.length < maxAmount) {
-        var newName = droneName + '-' + Game.time;
-        Game.spawns['Hatchery'].spawnCreep(droneBody, newName, 
-            {memory: {role: droneRole}});        
-    }
-    
-    if(Game.spawns['Hatchery'].spawning) { 
-        var spawningCreep = Game.creeps[Game.spawns['Hatchery'].spawning.name];
-        Game.spawns['Hatchery'].room.visual.text(
-            spawningCreep.memory.role,
-            Game.spawns['Hatchery'].pos.x + 1, 
-            Game.spawns['Hatchery'].pos.y, 
-            {align: 'left', opacity: 0.8});
-    }
-
-}
---
+++ spawning.js
@@ -0,0 +1,21 @@
+var spawningPool = function (droneName, droneRole, maxAmount, droneBody) {
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == droneRole);
+
+    if(harvesters.length < maxAmount) {
+        var newName = droneName + '-' + Game.time;
+        Game.spawns['Hatchery'].spawnCreep(droneBody, newName, 
+            {memory: {role: droneRole}});        
+    }
+    
+    if(Game.spawns['Hatchery'].spawning) { 
+        var spawningCreep = Game.creeps[Game.spawns['Hatchery'].spawning.name];
+        Game.spawns['Hatchery'].room.visual.text(
+            spawningCreep.memory.role,
+            Game.spawns['Hatchery'].pos.x + 1, 
+            Game.spawns['Hatchery'].pos.y, 
+            {align: 'left', opacity: 0.8});
+    }
+
+}
+
+module.exports = spawningPool;
