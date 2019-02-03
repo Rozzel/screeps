ec5d6dbd44b044caefbf11587d4b1d15e0dfc37e
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-03 11:38:02 +0300

    sporeCrawlerTower

--- towers.js
+++ towers.js
@@ -2,9 +2,9 @@ var sporeCrawlerTower = function () {
     var tower_1 = Game.getObjectById('5c55df5e6f956a230b9ebfe8');
 
     var towerFind = function (tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+        let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax});
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     
         if (closestHostile) {
             tower.attack(closestHostile);
