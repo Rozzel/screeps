6e7f4d93fc818dcb2faa20b1b1af9b311a34f165
Vladislav Golik <Vladislav.Golik@gmail.com>
2019-02-04 21:15:03 +0300

    drone

--- main.js
+++ main.js
@@ -10,17 +10,15 @@ sporeCrawlerTower();
 
 spawningPool('droneUpgraderBig', 'upgrader', 0, [WORK,WORK,CARRY,MOVE,MOVE,MOVE]);
 spawningPool('droneBuilderBig', 'builder', 0, [WORK,WORK,CARRY,MOVE]);
-spawningPool('droneUpgrader', 'upgrader', 1, [WORK,WORK,CARRY,MOVE]);
-spawningPool('droneHarvester', 'harvester', 8, [WORK,CARRY,MOVE]);
-spawningPool('droneHarvesterBig', 'harvester', 1, [WORK,MOVE,CARRY,CARRY]);
-spawningPool('droneBuilder', 'builder', 4, [WORK,CARRY,MOVE]);
+spawningPool('droneHarvesterBig', 'harvester', 0, [WORK,MOVE,CARRY,CARRY]);
+spawningPool('droneUpgrader', 'upgrader', 1, [WORK,WORK,CARRY,MOVE]); // 100 + 100 + 50 + 50 = 300
+spawningPool('droneHarvester', 'harvester', 8, [WORK,CARRY,MOVE]); // 100 + 50 + 50 = 200
+spawningPool('droneBuilder', 'builder', 4, [WORK,CARRY,MOVE]); // 100 + 50 + 50 = 200
 
 unitsRole();
 
 
 }
-
-
 
 var unitsRole = function () {
     for(var name in Game.creeps) {
