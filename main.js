07662abb9d7e7e3805ff148f604ca46b946a290d
Vladislav <rozzel@mail.ru>
2019-02-05 13:35:28 +0300

    unitsRole

--- main.js
+++ main.js
@@ -17,5 +17,18 @@ unitsRole();
 
 }
 
-
+var unitsRole = function () {
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+    }
+}
 
