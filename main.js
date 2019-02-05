6c20e70d06bf0bb21ed6ec38994c921c33343fd2
Vladislav <rozzel@mail.ru>
2019-02-05 13:31:05 +0300

    unitsRole

--- main.js
+++ main.js
@@ -12,23 +12,10 @@ spawningPool('droneUpgrader', 'upgrader', 1, [WORK,WORK,CARRY,CARRY,MOVE]); // 1
 spawningPool('droneHarvester', 'harvester', 8, [CARRY,WORK,CARRY,MOVE]); // 50+100+50+50=200
 spawningPool('droneBuilder', 'builder', 4, [WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE]); // 100+50+50+50+50+50+50=400
 
-unitsRole();
+
 
 
 }
 
-var unitsRole = function () {
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-    }
-}
+
 
--- spawning.js
+++ spawning.js
@@ -26,6 +26,22 @@ var spawningPool = function (droneName, droneRole, maxAmount, droneBody) {
     }
     deleteCreepsMemory();
 
+    var unitsRole = function () {
+        for(var name in Game.creeps) {
+            var creep = Game.creeps[name];
+            if(creep.memory.role == 'harvester') {
+                roleHarvester.run(creep);
+            }
+            if(creep.memory.role == 'builder') {
+                roleBuilder.run(creep);
+            }
+            if(creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
+            }
+        }
+    }
+    unitsRole();
+
 };
 
 module.exports = spawningPool;
