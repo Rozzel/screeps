36c7f7d8782dfde1214883d2c8660e43e02b4c47
Vladislav <rozzel@mail.ru>
2019-02-06 13:27:58 +0300

    creep.memory.role

--- main.js
+++ main.js
@@ -12,22 +12,5 @@ spawningPool('droneUpgrader', 'upgrader', 1, [WORK,WORK,CARRY,CARRY,MOVE]); // 1
 spawningPool('droneHarvester', 'harvester', 8, [MOVE,WORK,CARRY,MOVE]); // 50+100+50+50=200
 spawningPool('droneBuilder', 'builder', 4, [WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE]); // 100+50+50+50+50+50+50=400
 
-unitsRole();
 
-
-}
-
-var unitsRole = function () {
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if(creep.memory.role == 'builder') {
-            roleBuilder.run(creep);
-        }
-        if(creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
-        }
-    }
 }
