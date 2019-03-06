c21e52df4c06a007df09ca5bb1e0e4ad6a621e47
Vladislav <rozzel@mail.ru>
2019-03-06 13:41:51 +0300

    roleBuilder

--- _units.js
+++ _units.js
@@ -86,6 +86,40 @@ let units = {
 		}
 	},
 
+	roleBuilder(creep) {
+
+		if (creep.memory.building && creep.carry.energy == 0) {
+			creep.memory.building = false;
+			creep.say('harvest');
+		}
+		if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+			creep.memory.building = true;
+			creep.say('build');
+		}
+
+		if (creep.memory.building) {
+			let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+			if (targets.length) {
+				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(targets[0], {
+						visualizePathStyle: {
+							stroke: '#ffffff'
+						}
+					});
+				}
+			}
+		} else {
+			let sources = creep.room.find(FIND_SOURCES);
+			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(sources[0], {
+					visualizePathStyle: {
+						stroke: '#ffaa00'
+					}
+				});
+			}
+		}
+	},
+
 
 	roleUpgrader(creep) {
 
