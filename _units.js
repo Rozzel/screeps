b05011dc837dc115bc234ddf992de1fa2f3de729
Vladislav <rozzel@mail.ru>
2019-03-06 12:42:00 +0300

    droneСarrier

--- _units.js
+++ _units.js
@@ -2,46 +2,44 @@ let units = {
 
 	spawning(spawnName, droneName, droneRole, maxAmount, droneBody) {
 		let harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == droneRole);
-	
+
 		if (harvesters.length < maxAmount) {
-				let newName = `${droneName}-${Game.time}`;
-				Game.spawns[spawnName].spawnCreep(droneBody, newName, {
-						memory: {
-								role: droneRole
-						}
+			let newName = `${droneName}-${Game.time}`;
+			Game.spawns[spawnName].spawnCreep(droneBody, newName, {
+				memory: {
+					role: droneRole
+				}
+			});
+		}
+
+		if (Game.spawns[spawnName].spawning) {
+			let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
+			Game.spawns[spawnName].room.visual.text(
+				spawningCreep.memory.role,
+				Game.spawns[spawnName].pos.x + 1,
+				Game.spawns[spawnName].pos.y, {
+					align: 'left',
+					opacity: 0.8
 				});
 		}
-	
-		if (Game.spawns[spawnName].spawning) {
-				let spawningCreep = Game.creeps[Game.spawns[spawnName].spawning.name];
-				Game.spawns[spawnName].room.visual.text(
-						spawningCreep.memory.role,
-						Game.spawns[spawnName].pos.x + 1,
-						Game.spawns[spawnName].pos.y, {
-								align: 'left',
-								opacity: 0.8
-						});
-		}
-	
+
 		for (let name in Memory.creeps) {
-				if (!Game.creeps[name]) {
-						delete Memory.creeps[name];
-						console.log('Clearing non-existing creep memory:', name);
-				}
+			if (!Game.creeps[name]) {
+				delete Memory.creeps[name];
+				console.log('Clearing non-existing creep memory:', name);
+			}
 		};
-	
+
 	},
 
-	roleHarvester(creep) {
+	roleСarrier(creep) {
 
 		if (creep.carry.energy < creep.carryCapacity) {
-			let sources = creep.room.find(FIND_SOURCES);
-			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-				creep.moveTo(sources[0], {
-					visualizePathStyle: {
-						stroke: '#ffaa00'
-					}
-				});
+			let target = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+			if (target) {
+				if (creep.pickup(target) == ERR_NOT_IN_RANGE) {
+					creep.moveTo(target);
+				}
 			}
 		} else {
 			let targets = creep.room.find(FIND_STRUCTURES, {
@@ -77,15 +75,8 @@ let units = {
 		}
 
 		if (creep.memory.building) {
-			let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-			if (targets.length) {
-				if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-					creep.moveTo(targets[0], {
-						visualizePathStyle: {
-							stroke: '#ffffff'
-						}
-					});
-				}
+			for (let resourceType in creep.carry) {
+				creep.drop(resourceType);
 			}
 		} else {
 			let sources = creep.room.find(FIND_SOURCES);
@@ -102,27 +93,34 @@ let units = {
 
 	roleUpgrader(creep) {
 
-		if(creep.memory.upgrading && creep.carry.energy == 0) {
-				creep.memory.upgrading = false;
-				creep.say('harvest');
+		if (creep.memory.upgrading && creep.carry.energy == 0) {
+			creep.memory.upgrading = false;
+			creep.say('harvest');
 		}
-		if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-				creep.memory.upgrading = true;
-				creep.say('upgrade');
+		if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+			creep.memory.upgrading = true;
+			creep.say('upgrade');
 		}
 
-		if(creep.memory.upgrading) {
-				if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-				}
+		if (creep.memory.upgrading) {
+			if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(creep.room.controller, {
+					visualizePathStyle: {
+						stroke: '#ffffff'
+					}
+				});
+			}
+		} else {
+			let sources = creep.room.find(FIND_SOURCES);
+			if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+				creep.moveTo(sources[0], {
+					visualizePathStyle: {
+						stroke: '#ffaa00'
+					}
+				});
+			}
 		}
-		else {
-				let sources = creep.room.find(FIND_SOURCES);
-				if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-				}
-		}
-},
+	},
 
 
 
--- main.js
+++ main.js
@@ -1,34 +1,35 @@
-const tower = require('_towers'),
-      units = require('_units');
+const   tower = require('_towers'),
+        units = require('_units');
 
-module.exports.loop = () => { 
+module.exports.loop = () => {
 
-tower('5c55df5e6f956a230b9ebfe8');
-tower('5c57064bb9349812e0eca6d0');
-
-
-units.spawning('Hatchery', 'droneUpgrader', 'upgrader', 5, [WORK,CARRY,CARRY,MOVE,MOVE,MOVE]);
-units.spawning('Hatchery', 'droneHarvester', 'harvester', 12, [MOVE,WORK,CARRY,MOVE]);
-units.spawning('Hatchery', 'droneBuilder', 'builder', 0, [WORK,CARRY,CARRY,MOVE]);
+    tower('5c55df5e6f956a230b9ebfe8');
+    tower('5c57064bb9349812e0eca6d0');
 
 
+    // units.spawning('Hatchery', 'droneUpgrader', 'upgrader', 5, [WORK, CARRY, CARRY, MOVE, MOVE, MOVE]);
+    units.spawning('Hatchery', 'droneСarrier', 'сarrier', 3, [MOVE, CARRY,CARRY, MOVE, MOVE, MOVE]);
+    units.spawning('Hatchery', 'droneHarvester', 'harvester', 3, [WORK,WORK,CARRY,CARRY,MOVE]);
+    // units.spawning('Hatchery', 'droneBuilder', 'builder', 0, [WORK,WORK,CARRY,CARRY,MOVE]);
 
-( () => {
-    for(let name in Game.creeps) {
+
+
+
+    for (let name in Game.creeps) {
         let creep = Game.creeps[name];
-        if(creep.memory.role == 'harvester') {
-            units.roleHarvester(creep);
+        if (creep.memory.role == 'harvester') {
+            units.roleСarrier(creep);
         }
-        if(creep.memory.role == 'builder') {
+        if (creep.memory.role == 'harvester') {
+            units.roleСarrier(creep);
+        }
+        if (creep.memory.role == 'builder') {
             units.roleBuilder(creep);
         }
-        if(creep.memory.role == 'upgrader') {
+        if (creep.memory.role == 'upgrader') {
             units.roleUpgrader(creep);
         }
     }
-})();
 
 
 }
-
-
