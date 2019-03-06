ab65c67683f03db6a841f967ccea50b17cca3684
Vladislav <rozzel@mail.ru>
2019-03-06 14:30:34 +0300

    spawning

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ module.exports.loop = () => {
 
     units.spawning('Hatchery', 'droneUpgrader', 'upgrader', 1, [WORK, CARRY, CARRY, MOVE, MOVE, MOVE]);
     units.spawning('Hatchery', 'droneCarrier', 'carrier', 10, [MOVE, CARRY, MOVE, MOVE, MOVE]);
-    units.spawning('Hatchery', 'droneHarvester', 'harvester', 3, [WORK,WORK,CARRY,CARRY,MOVE]);
+    units.spawning('Hatchery', 'droneHarvester', 'harvester', 6, [WORK,WORK,CARRY,CARRY,MOVE]);
     units.spawning('Hatchery', 'droneBuilder', 'builder', 0, [WORK,WORK,CARRY,CARRY,MOVE]);
 
 
